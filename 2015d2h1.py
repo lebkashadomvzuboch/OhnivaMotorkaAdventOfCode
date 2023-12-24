89f74c5792e49a9eb5d9a0d9962d8ac5c1f9bf5e
lebkashadomvzuboch <154756862+lebkashadomvzuboch@users.noreply.github.com>
2023-12-24 21:56:53 +0100

    Update

---
+++ 2015d2h1.py
@@ -0,0 +1,10 @@
+total = 0
+
+with open("input.txt", "r") as subor:
+	for riadok in subor.readlines():
+		cisla = [int(i) for i in riadok.strip().split("x")]
+		m = [cisla[0]*cisla[1], cisla[0]*cisla[2], cisla[1]*cisla[2]]
+		
+		total += 2 * sum(m) + min(m)
+		
+print(total)
