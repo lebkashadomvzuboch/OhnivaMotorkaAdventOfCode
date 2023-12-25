64a4b4208e5e908e6e88332ed4ced663ab107db3
lebkashadomvzuboch <154756862+lebkashadomvzuboch@users.noreply.github.com>
2023-12-25 19:56:48 +0100

    Create 2015d4h2.py

---
+++ 2015d4h2.py
@@ -0,0 +1,11 @@
+import hashlib
+
+puzzle_input_ = "iwrupvqb"
+
+cislo = 0
+while True:
+    puzzle_input = f"{puzzle_input_}{cislo}"
+    cislo += 1
+    if hashlib.md5(puzzle_input.encode()).hexdigest().startswith("000000"):
+        print(cislo-1)
+        break
