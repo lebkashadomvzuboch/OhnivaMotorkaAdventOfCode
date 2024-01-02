4881f8599b7f3cf3be069354babe54b2b082323f
lebkashadomvzuboch <154756862+lebkashadomvzuboch@users.noreply.github.com>
2024-01-02 14:31:01 +0100

    Create 2022d4h1.py

---
+++ 2022/2022d4h1.py
@@ -0,0 +1,14 @@
+vysledok = 0
+
+with open("input_.txt", "r") as subor:
+    for riadok in subor.readlines():
+        pary = riadok.strip().split(",")
+        prvy_par, druhy_par = [int(i) for i in pary[0].split(
+            "-")], [int(i) for i in pary[1].split("-")]
+
+        if (prvy_par[0] <= druhy_par[0] and prvy_par[1] >= druhy_par[1]) or (druhy_par[0] <= prvy_par[0] and druhy_par[1] >= prvy_par[1]):
+            vysledok += 1
+
+print(vysledok)
+
+# vysledok je 431
