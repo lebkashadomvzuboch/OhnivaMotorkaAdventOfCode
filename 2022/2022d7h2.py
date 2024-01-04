ed2e126563c1d488d4e2cbeb1aaeea303fdc28be
lebkashadomvzuboch <154756862+lebkashadomvzuboch@users.noreply.github.com>
2024-01-04 13:43:24 +0100

    Create 2022d7h2.py

---
+++ 2022/2022d7h2.py
@@ -0,0 +1,52 @@
+from functools import cache
+aktualny_priecinok = None
+priecinky = dict()
+velkosti_priecinkov = dict()
+total = 0
+
+
+@cache
+def prehladaj_priecinok(priecinok):
+    total = 0
+    for vec in priecinky[priecinok]:
+        if isinstance(vec, int):
+            total += vec
+        else:
+            total += prehladaj_priecinok(vec)
+
+    return total
+
+
+with open("input_.txt") as subor:
+    for riadok in subor.readlines():
+        if riadok.startswith("$ cd "):
+            kam = riadok[5:].strip()
+            if aktualny_priecinok:
+                if kam == "..":
+                    aktualny_priecinok = aktualny_priecinok[0:aktualny_priecinok.rfind(
+                        "/")]
+                else:
+                    aktualny_priecinok += "/" + kam
+            else:
+                aktualny_priecinok = kam
+
+        else:
+            if not riadok.startswith("$"):
+                if not aktualny_priecinok in priecinky:
+                    priecinky[aktualny_priecinok] = []
+
+                if riadok.startswith("dir "):
+                    priecinky[aktualny_priecinok].append(
+                        aktualny_priecinok + "/" + riadok.strip().split("dir ")[1])
+                else:
+                    priecinky[aktualny_priecinok].append(
+                        int(riadok.strip().split(" ")[0]))
+
+for priecinok in priecinky:
+    v = prehladaj_priecinok(priecinok)
+    velkosti_priecinkov[priecinok] = v
+
+treba = 30000000 - (70000000 - velkosti_priecinkov["/"])
+print(min([v for v in velkosti_priecinkov.values() if v >= treba]))
+
+# vysledok je 3696336
