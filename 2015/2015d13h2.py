daf26723a093d63f9146749ac35f458c9ce6fc95
lebkashadomvzuboch <154756862+lebkashadomvzuboch@users.noreply.github.com>
2024-01-14 13:39:38 +0100

    Create 2015d13h2.py

---
+++ 2015/2015d13h2.py
@@ -0,0 +1,38 @@
+import itertools
+
+ludia_hodnoty = dict()
+
+with open("input.txt", "r") as subor:
+    for riadok in subor.readlines():
+        info = riadok.strip().strip(".").split(" ")
+        osoba1, zisk, cislo, osoba2 = info[0], info[2], int(info[3]), info[10]
+
+        if osoba1 in ludia_hodnoty:
+            ludia_hodnoty[osoba1][osoba2] = cislo if zisk == "gain" else -cislo
+        else:
+            ludia_hodnoty[osoba1] = {
+                osoba2: cislo if zisk == "gain" else -cislo, "ja": 0}
+
+
+ludia = set(ludia_hodnoty)
+ludia.add("ja")
+moznosti_usadenia = set(itertools.permutations(ludia))
+ludia_hodnoty["ja"] = {osoba: 0 for osoba in ludia}
+
+vysledok = 0
+
+for moznost in moznosti_usadenia:
+    temp = 0
+    for index, clovek in enumerate(moznost):
+        if index != len(ludia) - 1:
+            temp += ludia_hodnoty[clovek][moznost[index+1]]
+            temp += ludia_hodnoty[moznost[index+1]][clovek]
+        else:
+            temp += ludia_hodnoty[clovek][moznost[0]]
+            temp += ludia_hodnoty[moznost[0]][clovek]
+
+    vysledok = max(vysledok, temp)
+
+print(vysledok)
+
+# vysledok je 725
