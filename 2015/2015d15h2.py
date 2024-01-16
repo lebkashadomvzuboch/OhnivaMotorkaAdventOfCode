0c49de37c5b9633766e153619bbeb190a3e79dff
lebkashadomvzuboch <154756862+lebkashadomvzuboch@users.noreply.github.com>
2024-01-16 09:22:42 +0100

    Update 2015

---
+++ 2015/2015d15h2.py
@@ -0,0 +1,40 @@
+import itertools
+limit = 100
+ingrediencie = dict()
+vysledok = 0
+
+with open("aoc/input.txt", "r") as subor:
+    for riadok in subor.readlines():
+        info = riadok.strip().split(": ")
+        meno = info[0]
+        udaje = [int(udaj.split(" ")[1]) for udaj in info[1].split(", ")]
+        
+        ingrediencie[meno] = udaje
+        
+        
+kolaciky = set(itertools.combinations_with_replacement(ingrediencie, limit))
+
+def vynasob(vl):
+    t = 1
+    for v in vl[0:4]:
+        if v > 0:
+            t *= v
+        else:
+            return 0
+        
+    return t
+
+for kolacik in kolaciky:
+    vlastnosti = [0, 0, 0, 0, 0]
+    
+    for ingrediencia in kolacik:
+        for i in range(5):
+            vlastnosti[i] += ingrediencie[ingrediencia][i]
+            
+    if vlastnosti[4] == 500:
+        vysledok = max(vysledok, vynasob(vlastnosti))
+    
+print(vysledok)
+            
+# vysledok je 15862900
+            
